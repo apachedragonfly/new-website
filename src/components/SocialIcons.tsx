dee9d65a5ef953bd824eb9c87513d583e985d2fe
apachedragonfly <100011406+apachedragonfly@users.noreply.github.com>
2025-05-16 23:00:48 -0230

    Social

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -16,13 +16,14 @@ interface SocialLink {
 }
 
 const socialLinks: SocialLink[] = [
-  { href: "https://spotify.com", icon: FaSpotify, label: "Spotify", colorClass: "hover:text-[#1DB954]" },
-  { href: "https://music.apple.com", icon: FaApple, label: "Apple Music", colorClass: "hover:text-[#A2AAAD]" }, // Color can be more specific if desired
-  { href: "https://youtube.com", icon: FaYoutube, label: "YouTube", colorClass: "hover:text-[#FF0000]" },
-  { href: "https://twitter.com", icon: FaXTwitter, label: "X (Twitter)", colorClass: "hover:text-[#1DA1F2]" }, // Or black for X
-  { href: "https://instagram.com", icon: FaInstagram, label: "Instagram", colorClass: "hover:text-[#E1306C]" },
-  { href: "https://tiktok.com", icon: FaTiktok, label: "TikTok", colorClass: "hover:text-[#00f2ea]" }, // TikTok has a few common colors
+  { href: "https://open.spotify.com/artist/6LgLJCfIgQ5VDL8J9F1sfv?si=sSQE05U3THStGVu0FuQkeg", icon: FaSpotify, label: "Spotify", colorClass: "hover:text-[#1DB954]" },
+  { href: "https://music.apple.com/ca/artist/jeremy-craig-saunders/1715078505", icon: FaApple, label: "Apple Music", colorClass: "hover:text-[#A2AAAD]" }, // Color can be more specific if desired
+  { href: "https://www.youtube.com/@jeremycraigsaunders", icon: FaYoutube, label: "YouTube", colorClass: "hover:text-[#FF0000]" },
+  { href: "https://x.com/JeremyOnX", icon: FaXTwitter, label: "X (Twitter)", colorClass: "hover:text-[#1DA1F2]" }, // Or black for X
+  { href: "https://instagram.com/jeremyspictures", icon: FaInstagram, label: "Instagram", colorClass: "hover:text-[#E1306C]" },
+  { href: "https://tiktok.com/jeremystoks", icon: FaTiktok, label: "TikTok", colorClass: "hover:text-[#00f2ea]" }, // TikTok has a few common colors
 ];
+
 
 const SocialIcons: React.FC = () => {
   return (
